19298db03c599abf4b1c709a9e54e0301e9ae7b8
Sava <savely54@mail.ru>
2021-08-03 14:36:06 +0700

    Added a slider

---
+++ src/components/Sliders/BtnSlider.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import rightArrow from './assets/right-arrow.svg'
+import leftArrow from './assets/left-arrow.svg'
+import './Sliders.scss'
+
+interface Props {
+  direction: 'next' | 'prev'
+  onClick: () => void
+}
+
+const BtnSlider: React.FC<Props> = ({ direction, onClick }) => {
+  return (
+    <button
+      onClick={onClick}
+      className={direction === 'next' ? 'btn-slide next' : 'btn-slide prev'}>
+      <img src={direction === 'next' ? rightArrow : leftArrow} />
+    </button>
+  )
+}
+
+export default BtnSlider
---
+++ src/components/Sliders/assets/pictures/pictureArray.ts
@@ -0,0 +1,8 @@
+import first from './1.jpg'
+import second from './2.jpeg'
+import third from './3.jpg'
+import fourth from './4.jpeg'
+import fifth from './5.jpeg'
+import sixth from './6.jpg'
+
+export const pictureArray = [first, second, third, fourth, fifth, sixth]
