72b8311ae677397dd195524187581d8eb889b3d5
Sava <savely54@mail.ru>
2021-08-03 14:50:23 +0700

    created a nav menu

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,34 @@
 import React from 'react'
 import './App.scss'
 
+import {
+  BrowserRouter as Router,
+  NavLink,
+  Route,
+  Switch,
+} from 'react-router-dom'
+import Sliders from '../Sliders/SliderOne/SliderOne'
+
 const App: React.FC = () => {
   return (
-    <div className="container">
-      <h1>Hello</h1>
-    </div>
+    <Router>
+      <div className="container">
+        <div className="links">
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/sliders">Sliders</NavLink>
+          <NavLink to="/drag_and_drop">Drag and Drop</NavLink>
+        </div>
+        <Switch>
+          <Route path="/sliders">
+            <Sliders />
+          </Route>
+          <Route path="/" exact>
+            This is my own outlines
+          </Route>
+          <Route path="/drag_and_drop">In progress...</Route>
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
