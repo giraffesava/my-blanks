916b982f19da27ddeedc9ef51f3b5ba98ca695c9
Sava <savely54@mail.ru>
2021-08-03 14:49:06 +0700

    Changed locations of files

---
+++ src/components/Sliders/SliderOne/SliderOne.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+import './Sliders.scss'
+import { pictureArray } from './assets/pictures/pictureArray'
+import BtnSlider from './BtnSlider'
+import { v4 as uuidv4 } from 'uuid'
+
+const SliderOne = () => {
+  const [sliderIndex, setSliderIndex] = useState(1)
+
+  const nextSlide = () => {
+    if (sliderIndex === pictureArray.length) {
+      setSliderIndex(1)
+    } else {
+      setSliderIndex((prev) => prev + 1)
+    }
+  }
+
+  const prevSlide = () => {
+    if (sliderIndex === 1) {
+      setSliderIndex(pictureArray.length)
+    } else if (sliderIndex !== 1) {
+      setSliderIndex((prev) => prev - 1)
+    }
+  }
+
+  const moveDot = (index) => {
+    setSliderIndex(index)
+  }
+  return (
+    <div className="slider-container">
+      {pictureArray.map((picture, index) => (
+        <div
+          key={uuidv4()}
+          className={sliderIndex === index + 1 ? 'slide active-anim' : 'slide'}>
+          <img src={picture} />
+        </div>
+      ))}
+      <BtnSlider onClick={prevSlide} direction="prev" />
+      <BtnSlider onClick={nextSlide} direction="next" />
+      <div className="container-dots">
+        {Array.from({ length: pictureArray.length }).map((_, index) => (
+          <div
+            key={uuidv4()}
+            className={sliderIndex === index + 1 ? 'dot active' : 'dot'}
+            onClick={() => moveDot(index + 1)}></div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default SliderOne
--- src/components/Sliders/Sliders.tsx
+++ src/components/Sliders/Sliders.tsx
@@ -1,52 +1,8 @@
-import React, { useState } from 'react'
-import './Sliders.scss'
-import { pictureArray } from './assets/pictures/pictureArray'
-import BtnSlider from './BtnSlider'
-import { v4 as uuidv4 } from 'uuid'
+import React from 'react'
+import SliderOne from './SliderOne/SliderOne'
 
-const SliderTwo = () => {
-  const [sliderIndex, setSliderIndex] = useState(1)
-
-  const nextSlide = () => {
-    if (sliderIndex === pictureArray.length) {
-      setSliderIndex(1)
-    } else {
-      setSliderIndex((prev) => prev + 1)
-    }
-  }
-
-  const prevSlide = () => {
-    if (sliderIndex === 1) {
-      setSliderIndex(pictureArray.length)
-    } else if (sliderIndex !== 1) {
-      setSliderIndex((prev) => prev - 1)
-    }
-  }
-
-  const moveDot = (index) => {
-    setSliderIndex(index)
-  }
-  return (
-    <div className="slider-container">
-      {pictureArray.map((picture, index) => (
-        <div
-          key={uuidv4()}
-          className={sliderIndex === index + 1 ? 'slide active-anim' : 'slide'}>
-          <img src={picture} />
-        </div>
-      ))}
-      <BtnSlider onClick={prevSlide} direction="prev" />
-      <BtnSlider onClick={nextSlide} direction="next" />
-      <div className="container-dots">
-        {Array.from({ length: pictureArray.length }).map((_, index) => (
-          <div
-            key={uuidv4()}
-            className={sliderIndex === index + 1 ? 'dot active' : 'dot'}
-            onClick={() => moveDot(index + 1)}></div>
-        ))}
-      </div>
-    </div>
-  )
+const Sliders = () => {
+  return <SliderOne />
 }
 
-export default SliderTwo
+export default Sliders
